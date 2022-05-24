01317f273ecfadda23d192c6bcd77a5464e9a996
António Fagundes <antonio.fagundes@eclo.solutions>
2022-05-24 12:41:50 +0100

    Add back Rtc

--- M5StackCommon/Core2ToughCommon.cs
+++ M5StackCommon/Core2ToughCommon.cs
@@ -5,13 +5,13 @@
 
 #if M5CORE2
 using Iot.Device.Ft6xx6x;
-using Iot.Device.Rtc;
 using nanoFramework.M5Core2;
 #elif TOUGH
 using Iot.Device.Chs6540;
 using nanoFramework.Tough;
 #endif
 using Iot.Device.Axp192;
+using Iot.Device.Rtc;
 using nanoFramework.Hardware.Esp32;
 using nanoFramework.Runtime.Native;
 using System;
@@ -31,12 +31,12 @@ namespace nanoFramework.M5Stack
 #endif
     {
         private const int TouchPinInterrupt = 39;
+        private static Pcf8563 _rtc;
         private static Axp192 _power;
         private static bool _powerLed;
         private static bool _vibrate;
 #if M5CORE2
         private static Ft6xx6x _touchController;
-        private static Pcf8563 _rtc;
 #elif TOUGH
         private static Chs6540 _touchController;
 #endif
@@ -67,7 +67,6 @@ namespace nanoFramework.M5Stack
         /// <remarks>Please make sure to read the documentation before adjusting any element.</remarks>
         public static Axp192 Power { get => _power; }
 
-#if M5CORE2
         /// <summary>
         /// Gets the real time clock.
         /// </summary>
@@ -76,6 +75,7 @@ namespace nanoFramework.M5Stack
             get => _rtc;
         }
 
+#if M5CORE2
         /// <summary>
         /// Sets on or off the Power Led.
         /// </summary>
@@ -342,7 +342,6 @@ namespace nanoFramework.M5Stack
             // The portA is the second I2C
             _portANumber = 2;
 
-#if M5CORE2
             // Setup the time if any
             _rtc = new Pcf8563(I2cDevice.Create(new I2cConnectionSettings(1, Pcf8563.DefaultI2cAddress)));
 
@@ -366,8 +365,6 @@ namespace nanoFramework.M5Stack
                     Rtc.SetSystemTime(dt);
                 }
             }
-#endif
-
         }
 
         /// <summary>
