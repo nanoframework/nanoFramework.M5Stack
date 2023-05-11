8ce760b93186f67aea174bdd60e056fd64ea9e7d
Laurent Ellerbach <laurelle@microsoft.com>
2023-05-11 20:17:47 +0200

    Fixing move to new graphic library (#273)
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- M5StackCommon/ScreenBase.cs
+++ M5StackCommon/ScreenBase.cs
@@ -1,9 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using nanoFramework.Presentation.Media;
 using nanoFramework.UI;
 using System.Device.Gpio;
+using System.Drawing;
 
 namespace nanoFramework.M5Stack
 {
